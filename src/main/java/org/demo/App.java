0c66a66a118a2b2dfe66f18046c0fd0e0d1c460f
Igor Adulyan <53629576+igoradulian@users.noreply.github.com>
2023-09-16 19:33:14 -0700

    Main branch is initial set up for sub-demo projects. You can use this branch as skeleton for your own projects

---
+++ src/main/java/org/demo/App.java
@@ -0,0 +1,22 @@
+package org.demo;
+
+/**
+ * @author Igor Adulyan
+ */
+
+
+/**
+ * Main benac is initial set up
+ * for sub-demo projects.
+ * You can use this branch as
+ * skeleton for your own projects
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
